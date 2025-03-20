f53404c90ccd1c23b29996538ffa79f6a8bd17d4
Dmytro <dimabokov5@gmail.com>
2025-03-20 14:37:48 +0000

    Fix: Only three posts now can be added to a recent posts section

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,8 +11,10 @@ class PagesController extends Controller
 {
     public function index()
     {
-        // Fetch posts created in the last 7 days
+        // Fetch the last 3 posts created in the last 7 days
         $recentPosts = Post::where('created_at', '>=', Carbon::now()->subDays(7))
+            ->orderBy('created_at', 'desc') // Order by most recent
+            ->take(3) // Limit to 3 posts
             ->get()
             ->map(function ($post) {
                 $post->description = Str::limit($post->description, 500);
